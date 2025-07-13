b9441a4e0db2c66c7d4ebbf46aae0016813f0a03
sanjit singh <ss3448@scarletmail.rutgers.edu>
2025-07-13 18:52:17 -0400

    found a second model with voxelization shaders for point cloud in vr

---
+++ Assets/Effects/Mesh Distortion/DistortionMeshInputControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class DistortionMeshInputControl : MonoBehaviour
+{
+    [SerializeField] DistortionMesh DistortionMesh;
+    Tween Increasetween;
+    Tween Decreasetween;
+
+    bool inputFlag;
+
+    private void Awake()
+    {
+        InitialDoTween();
+    }
+
+    private void InitialDoTween()
+    {
+        Increasetween = DOTween.To(ChaosStep, 0, 1, 5).SetEase(Ease.InOutSine);
+        Decreasetween = DOTween.To(ChaosStep, 1, 0, 5).SetEase(Ease.InOutSine);
+
+        Decreasetween.Pause();
+        Increasetween.Pause();
+
+        Increasetween.SetAutoKill(false);
+        Decreasetween.SetAutoKill(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            InputControl();
+        }
+    }
+
+    private void InputControl()
+    {
+        if(!Increasetween.IsPlaying() && !Decreasetween.IsPlaying())
+        {
+            if (inputFlag)
+            {
+                inputFlag = false;
+                Decreasetween.Restart();
+            }
+            else
+            {
+                inputFlag = true;
+                Increasetween.Restart();
+            }
+        }
+    }
+
+    void ChaosStep(float step)
+    {
+        DistortionMesh.Chaos = Mathf.Lerp(0, 1000, step);
+    }
+}
